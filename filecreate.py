64bfe4d762ee06982e9bd5347c7b6032b06525bd
ashwinraok <ashwinbharath974@gmail.com>
2026-09-21 18:21:33 +0530

    added pdf regex

--- filecreate.py
+++ filecreate.py
@@ -1,5 +1,6 @@
 with open("test.pdf", "w") as file:
     file.write("Hello, World!\n")
-    file.write("This file was created using Python file handling.")
+    file.write("This file was created using Python file handling.\n")
+    file.write("Added third line.")
 
 print("File created successfully!")
